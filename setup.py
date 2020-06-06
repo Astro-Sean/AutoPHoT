f22992262f6cab54c04a4813ae3d9fb55322ada3
Seán Brennan <brennan4661@hotmail.com>
2020-06-06 15:16:46 +0100

    setup example for notebook

--- setup.py
+++ setup.py
@@ -6,16 +6,15 @@ setuptools.setup(
     author="Sean Brennan",
     author_email="sean.brennan2@ucdconnect.ie",
     description="Automated Photometry of Transients",
+
     url='https://github.com/Astro-Sean/autophot',
+    
     long_description='Automated Pipelines for quick and accurate photometry of astronomical transient events',
     packages=setuptools.find_packages(),
     python_requires='>=3.5, <4',
-    package_data={'autophot': ['databases/*.yml','example/example.fits']
+    package_data={'': ['databases/*.yml','example/example.fits']
                   },
-
-
     classifiers=[
-
         'Development Status :: 3 - Alpha',
 
         # WHo project is intended for
@@ -31,7 +30,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.8',
         ],
 
-    project_urls={  # Optional
+    project_urls={
         'Bug Reports': 'https://github.com/Astro-Sean/autophot/issues',
         'Source': 'https://github.com/Astro-Sean/autophot',
         'Homepage':'https://sn.ie'
